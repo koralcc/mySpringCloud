56db605d935e0dfec444f82bcfe7693fdd8b6e00
koral <warehousecjy@163.com>
2022-09-16 23:05:28 +0800

    1. 本地推送远程git仓库，报openssl报错，本地“git config --global http.sslVerify "false"” 2. 对于nacos2.0 需要另外开放两个端口9848和9849，映射到外部端口，并开启防火墙即可   docker启动命令   docker run -d -p 8848:8848 -p 9848:9 848 -p 9849:9849 -e MODE=standalone -e PREFER_HOST_MODE=hostname -v /root/nacos/init.d/custom.properties:/home/nacos/init.d/custom.properties -v /root/nacos/logs:/home/nacos/logs --restart always --name nacos nacos/nacos-server 3. 本次提交是最小的注册成功案例

--- mySpringCloud/src/main/java/com/koral/App_Provider.java
+++ mySpringCloud/src/main/java/com/koral/App_Provider.java
@@ -2,10 +2,12 @@ package com.koral;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class App {
+@EnableDiscoveryClient
+public class App_Provider {
     public static void main(String[] args) {
-        SpringApplication.run(App.class);
+        SpringApplication.run(App_Provider.class);
     }
 }
