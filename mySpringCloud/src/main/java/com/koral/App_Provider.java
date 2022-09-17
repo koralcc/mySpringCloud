771a6357267e3b80682c6fb7ab2949c9a5e808d7
koral <warehousecjy@163.com>
2022-09-17 23:51:14 +0800

    1. 配置中心yaml/yml后缀配置文件    只需要在本地bootstrap文件添加yaml后缀：    spring.cloud.nacos.config.file-extension=yaml(默认是properties) 2. 动态读取配置中心数据，就是死循环读environment里面的属性值

--- mySpringCloud/src/main/java/com/koral/App_Provider.java
+++ mySpringCloud/src/main/java/com/koral/App_Provider.java
@@ -6,14 +6,20 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class App_Provider {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ConfigurableApplicationContext ctx = SpringApplication.run(App_Provider.class);
         ConfigurableEnvironment environment = ctx.getEnvironment();
-        String name = environment.getProperty("user.name");
-        String age = environment.getProperty("user.age");
-        System.out.println("name" +name+"age:"+age);
+        while (true){
+            //When configurations are refreshed dynamically, they will be updated in the Enviroment, therefore here we retrieve configurations from Environment every other second.
+            String userName = environment.getProperty("user.name");
+            String userAge = environment.getProperty("user.age");
+            System.err.println("user name :" + userName + "; age: " + userAge);
+            TimeUnit.SECONDS.sleep(1);
+        }
     }
 }
