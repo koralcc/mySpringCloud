6a4670332657260ca58d549a55cea8c66481dac5
koral <warehousecjy@163.com>
2022-09-17 23:29:48 +0800

    最小配置中心sample代码

--- mySpringCloud/src/main/java/com/koral/App_Provider.java
+++ mySpringCloud/src/main/java/com/koral/App_Provider.java
@@ -3,11 +3,17 @@ package com.koral;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class App_Provider {
     public static void main(String[] args) {
-        SpringApplication.run(App_Provider.class);
+        ConfigurableApplicationContext ctx = SpringApplication.run(App_Provider.class);
+        ConfigurableEnvironment environment = ctx.getEnvironment();
+        String name = environment.getProperty("user.name");
+        String age = environment.getProperty("user.age");
+        System.out.println("name" +name+"age:"+age);
     }
 }
